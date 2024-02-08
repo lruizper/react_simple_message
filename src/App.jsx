a5ae46cbd4bfa4086bd98ad854aefe008f04952c
lruizper <lruizper@gmail.com>
2024-02-08 17:00:08 +0800

    Update button text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // return <h1>Lanie in da house</h1>;
-  // if i want to use a variable i can use the curly brackets
-  // const mymessage = 'Lanie in da house <3'
-  
   const [mymessage, setMymessage] = React.useState('Lanie in da house <3')
   // return <h1>{mymessage}</h1>;
   function handleClick() {
     setMymessage('second message here')
   }
+  
   return (
 // java lets us use an empty tag to wrap multiple HLTM elements
 // this is useful because return can only take a single element
 // the trick is that it can contain multiple elements inside :D 
     <> 
       <h1>{mymessage}</h1>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClick}>update the message</button>
       <img src={reactLogo} alt="react logo" />
       <img src={viteLogo} alt="vite logo" />
     </>
   )
 }
 
-export default App
+export default App;
